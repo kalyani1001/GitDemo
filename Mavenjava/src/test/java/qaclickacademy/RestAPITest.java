81e0be6dc6d1d39366f20302531070c498937d06
kalyani <kalyaniyedupati@gmail.com>
2022-07-29 19:18:59 -0400

    first commit

---
+++ Mavenjava/src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void nativeApps()
+	{
+		System.out.println("NativeApps");
+	}
+
+	@Test
+	public void androidApps()
+	{
+		System.out.println("androidApps");
+	}
+}
